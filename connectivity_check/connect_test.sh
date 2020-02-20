d9a64fa51c91b7508159547a7251ca0f060ba8bd
Pete Bird <pbird@onica.com>
2020-02-20 17:43:34 -0600

    update readme, add input files

---
+++ connectivity_check/connect_test.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+resolver = "172.31.10.11"
+
+while read ip
+do
+  if nc -vzw5 "$ip" 22 > /dev/null 2>&1; then
+    echo "Success! Can reach $ip"
+  else
+    echo "D'oh! Can't reach $ip"
+  fi
+done < hosts/ip_list.txt
+
+while read host
+do
+  if nslookup "$host" "$resolver" > /dev/null; then
+    echo "Success! Can resolve $host"
+  else
+    echo "D'oh! Can't resolve $host"
+  fi
+done < hosts/host_list.txt
